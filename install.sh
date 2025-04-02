3b44b02cdeee56b209e7cf9d762a8534cba9d4dc
kvokka <kvokka@yahoo.com>
2025-04-03 01:08:00 +0700

    feat(install): automate dotfiles setup
    This commit automates the dotfiles setup process by installing chezmoi, homebrew, asdf and helm, applying chezmoi configurations, installing brew dependencies and asdf versions.

--- install.sh
+++ install.sh
@@ -3,3 +3,17 @@
 set -eu
 
 dotfiles_bootstrap/install_chezmoi.sh
+
+hash brew &> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+source ~/.zprofile
+
+brew install chezmoi
+chezmoi init --apply --force
+brew bundle --global
+
+source ~/.zshrc
+
+cat .tool-versions | while read v;do asdf plugin add $(cut -f1 -d' ' <<<$v);done
+asdf install
+
+command -v helm >/dev/null && helm plugin install https://github.com/databus23/helm-diff || true
