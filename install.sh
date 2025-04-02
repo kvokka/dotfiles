27cfd4656885d81edd6f92fe4262706c2cef588f
kvokka <kvokka@yahoo.com>
2025-04-03 04:42:18 +0700

    chore: simplify install script and brewfile
    This commit removes the separate chezmoi install script and integrates the installation logic directly into `install.sh`. It also adjusts the Brewfile to conditionally install certain packages based on the OS and architecture, optimizing the installation process for different environments.

--- install.sh
+++ install.sh
@@ -1,21 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eu
 
-dotfiles_bootstrap/install_chezmoi.sh
+command -v brew &> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-hash brew &> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-source ~/.zprofile
+case $(uname) in
+  [Dd]arwin*)
+    TMP_BREW_PATH="/opt/homebrew/bin/brew"
+    ;;
+  [Ll]inux*)
+    TMP_BREW_PATH="/home/linuxbrew/.linuxbrew/bin/brew"
+    go install github.com/asdf-vm/asdf/cmd/asdf@v0.16.7
+    ;;
+  *)
+    echo "Unsupported operating system: '$(uname)'"; exit 1;;
+esac
+[ -f "$TMP_BREW_PATH" ] && eval "$($TMP_BREW_PATH shellenv)" && unset TMP_BREW_PATH
 
-brew install chezmoi
-chezmoi init --apply --force
+sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply --force --no-tty --debug --purge-binary kvokka
+tree -a
 brew bundle --global
 
 source ~/.zshrc
 
 cat .tool-versions | while read v;do asdf plugin add $(cut -f1 -d' ' <<<$v);done
-asdf install
+asdf install && asdf list
 
-command -v helm >/dev/null && helm plugin install https://github.com/databus23/helm-diff || true
+command -v helm &>/dev/null && helm plugin install https://github.com/databus23/helm-diff || true
 
 echo ">>> Installation is done!"
