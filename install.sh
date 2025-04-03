40ff5c6463e857d2eef3fba318778796ddef3af1
kvokka <kvokka@yahoo.com>
2025-04-03 19:15:20 +0700

    refactor: simplify install script using chezmoi init
    Refactors the install script to use `chezmoi init` with a source directory, simplifying the installation process and leveraging chezmoi for file management.  Also improves chezmoi installation logic.

--- install.sh
+++ install.sh
@@ -2,34 +2,26 @@
 
 set -eu
 
-command -v brew &> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
 
-case $(uname) in
-  [Dd]arwin*)
-    TMP_BREW_PATH="/opt/homebrew/bin/brew"
-    ;;
-  [Ll]inux*)
-    TMP_BREW_PATH="/home/linuxbrew/.linuxbrew/bin/brew"
-    # TODO: Kill this installation spike after brew 4.5 release
-    go install github.com/asdf-vm/asdf/cmd/asdf@v0.16.7
-    ;;
-  *)
-    echo "Unsupported operating system: '$(uname)'"; exit 1;;
-esac
-[ -f "$TMP_BREW_PATH" ] && eval "$($TMP_BREW_PATH shellenv)" && unset TMP_BREW_PATH
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply --verbose --no-tty --purge-binary kvokka
-tree -a
-brew bundle --global
+eval "$chezmoi" init --apply "--source=$script_dir"
 
-export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
-asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
-cat .tool-versions | while read v;do asdf plugin add $(cut -f1 -d' ' <<<$v);done
-asdf install && asdf list
-
-command -v helm &>/dev/null && helm plugin install https://github.com/databus23/helm-diff || true
-
-chezmoi apply
+tree -a -L 2 "$HOME"
 
 echo ">>> Installation is done!"
