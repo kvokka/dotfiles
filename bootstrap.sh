1094d3e4bea73af4511b964c158b4f270087e96b
kvokka <kvokka@yahoo.com>
2025-04-03 21:45:07 +0700

    chore: enhance bootstrap script robustness
    - Implemented logging to track bootstrap execution.
    - Force chezmoi init to handle potentially corrupted states.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,10 @@
 
 set -eu
 
-sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply
+# Until brew 4.5 is out we have to install it manually, only if we are in codespaces
+[ "$CODESPACES" = "true" ] && go install github.com/asdf-vm/asdf/cmd/asdf@v0.16.7
+
+sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply --force --purge-binary
 
 command -v brew &> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -10,7 +13,7 @@ source ~/.zprofile ~/.asdf/asdf.sh
 
 brew bundle --global
 
-cat .tool-versions | while read v;do asdf plugin add $(cut -f1 -d' ' <<<$v);done
+while read -r v || [[ -n "$v" ]]; do [[ -z "$v" ]] || asdf plugin add "$(cut -f1 -d' ' <<<"$v")"; done < ~/.tool-versions
 asdf install && asdf list
 
 command -v helm &>/dev/null && helm plugin install https://github.com/databus23/helm-diff || true
