86bde6663a3738b2491a4bb59dcc54758adf231f
kvokka <kvokka@yahoo.com>
2025-04-05 12:24:59 +0700

    fix: avoid interactive prompt in brew install
    Set NONINTERACTIVE=1 to prevent prompts during brew installation in non-interactive environments like CI or automated setups. This ensures smooth, unattended execution.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply --force --purge-binary
 # Until brew 4.5 is out we have to install it manually, only if we are in codespaces
 [ "$CODESPACES" = "true" ] && go install github.com/asdf-vm/asdf/cmd/asdf@v0.16.7
 
-command -v brew &> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+command -v brew &> /dev/null || NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 source ~/.zprofile ~/.asdf/asdf.sh
 
