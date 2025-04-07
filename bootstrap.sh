3c750645a2bb839f71156be3b163a42759cbc13a
kvokka <kvokka@yahoo.com>
2025-04-07 17:58:58 +0700

    chore: improve bootstrap script execution
    Ensures zprofile is sourced before installing dependencies.
    Loads asdf after brew bundle for correct environment setup.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,14 @@ sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply --force --purge-binary
 
 command -v brew &> /dev/null || NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+source ~/.zprofile
+
 # Until brew 4.5 is out we have to install it manually, only if we are in codespaces
 [ "$CODESPACES" = "true" ] && brew install --build-from-source go asdf
 
-source ~/.zprofile ~/.asdf/asdf.sh
-
 brew bundle --global
 
+source ~/.asdf/asdf.sh
 while read -r v || [[ -n "$v" ]]; do [[ -z "$v" ]] || asdf plugin add "$(cut -f1 -d' ' <<<"$v")"; done < ~/.tool-versions
 asdf install && asdf list
 
