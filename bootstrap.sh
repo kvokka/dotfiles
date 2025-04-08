96bbf0bacc00d4c4e6daad3452a675750cd416a9
kvokka <kvokka@yahoo.com>
2025-04-09 03:07:34 +0700

    feat: replace asdf with mise for tool management
    This commit replaces asdf with mise for managing tool versions.
    Mise offers improved performance and a simpler configuration.
    
    - Removes asdf-related configurations and dependencies.
    - Adds mise configuration file and integrates it into the shell environment.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,16 +5,10 @@ set -eu
 sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply --force --purge-binary
 
 command -v brew &> /dev/null || NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-source ~/.zprofile
-
-# Until brew 4.5 is out we have to install it manually, only if we are in codespaces
-[ "$CODESPACES" = "true" ] && brew install --build-from-source go asdf
+source ~/.zsh/homebrew.zsh
 
 brew bundle --global
-
-while read -r v || [[ -n "$v" ]]; do [[ -z "$v" ]] || asdf plugin add "$(cut -f1 -d' ' <<<"$v")"; done < ~/.tool-versions
-asdf install && asdf list
+mise install
 
 command -v helm &>/dev/null && helm plugin install https://github.com/databus23/helm-diff || true
 
