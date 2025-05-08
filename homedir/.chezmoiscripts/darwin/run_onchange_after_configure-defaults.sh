20522138dfd59ee9552ce3165ca50aba80a7a321
kvokka <kvokka@yahoo.com>
2025-05-08 22:26:27 +0700

    feat(darwin): configure macOS defaults
    Adds a script to configure various macOS defaults, including disabling miniaturize on double click, press and hold, and enabling some automatic features.

---
+++ homedir/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+defaults write -g AppleMiniaturizeOnDoubleClick -int 0
+defaults write -g ApplePressAndHoldEnabled -int 0
+defaults write -g InitialKeyRepeat -int 25
+defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -float 0.4
+defaults write -g KeyRepeat -float 0.016666
+defaults write -g NSAutomaticCapitalizationEnabled -int 1
+defaults write -g NSAutomaticDashSubstitutionEnabled -int 1
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -int 1
+defaults write -g NSAutomaticSpellingCorrectionEnabled -int 1
+defaults write -g NSAutomaticQuoteSubstitutionEnabled -int 1
+defaults write -g NSDocumentSaveNewDocumentsToCloud -int 0
+defaults write -g WebAutomaticSpellingCorrectionEnabled -int 1
+defaults write -g com.apple.swipescrolldirection -int 0
+defaults write -g com.apple.trackpad.forceClick -int 0
+
+defaults write com.apple.dock autohide -int 0
+defaults write com.apple.dock largesize -int 128
+
+defaults write com.apple.finder FXPreferredViewStyle -string Nlsv
+defaults write com.apple.finder FXRemoveOldTrashItems -int 1
