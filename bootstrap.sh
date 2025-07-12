eef6e353fd6469a925dbcfccad91f34b6c55fa84
kvokka <kvokka@yahoo.com>
2025-07-13 03:17:04 +0700

    fix(macos): improve Homebrew installation robustness
    Adjust the Homebrew installation command to conditionally set the `NONINTERACTIVE` flag. This ensures a smoother installation experience across various environments, including interactive and non-interactive shell sessions, particularly on macOS.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,11 @@
 
 set -eu
 
+[[ ! -t 0 ]] && export NONINTERACTIVE=1 # Allow user to enter sudo pass for brew setup on mac
+command -v brew &> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 command -v chezmoi &>/dev/null || sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply --force --purge-binary ${@}
 
-command -v brew &> /dev/null || NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 command -v compaudit &> /dev/null && compaudit | xargs chmod g-w
 source ~/.zsh/homebrew.zsh
 
