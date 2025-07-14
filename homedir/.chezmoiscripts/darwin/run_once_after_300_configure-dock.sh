d52021aed68a9f19e611cd0b341875421d2987f7
kvokka <kvokka@yahoo.com>
2025-07-15 00:17:49 +0700

    chore(chezmoi): change dock script to run once
    The `configure-dock.sh` script is now configured to run only once by `chezmoi` instead of on every change.
    
    This is because dock configuration is typically a one-time setup and does not require re-execution unless explicitly modified, improving `chezmoi` apply efficiency.

--- homedir/.chezmoiscripts/darwin/run_once_after_300_configure-dock.sh
+++ homedir/.chezmoiscripts/darwin/run_once_after_300_configure-dock.sh
@@ -30,8 +30,10 @@ for label in "${remove_labels[@]}"; do
 	dockutil --no-restart --remove "${label}" || true
 done
 
-dockutil --add /Applications/Visual\ Studio\ Code.app --position 1 &>/dev/null
+dockutil --no-restart --add /Applications/Visual\ Studio\ Code.app --position 1 &>/dev/null
 
-dockutil --add /Applications/Google\ Chrome.app --position 2 &>/dev/null
+dockutil --no-restart --add /Applications/Google\ Chrome.app --position 2 &>/dev/null
 
 dockutil --add /Applications/iTerm.app/ --position 3 &>/dev/null
+
+true
